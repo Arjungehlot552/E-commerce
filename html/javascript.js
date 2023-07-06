1371e9fd7530439758199ba64176dc89a3ebb6e4
Arjungehlot552 <arjungehlot552@gmail.com>
2023-07-07 02:23:28 +0530

    my e-commerce

---
+++ html/javascript.js
@@ -0,0 +1,39 @@
+//array of the object//
+
+// const topoffers = [
+//     {
+//         imgurl : "https://rukminim1.flixcart.com/image/200/200/xif0q/screen-guard/edge-to-edge-tempered-glass/5/t/b/gsaug02-gorilla-cases-original-imaghkdfngmybbsg.jpeg?q=70",
+//            name : "Tempered Glass",
+//            price : 99,
+//            brand : "For All Top Models"
+//     },
+//     {
+//         imgurl : "https://rukminim1.flixcart.com/image/200/200/ktop5e80/tablet/x/9/o/mk2k3hn-a-apple-original-imag6yy7xjjugz4w.jpeg?q=70",
+//            name : "Apple iPads",
+//            price : 20000,
+//            brand : "Liquid Retina Display"
+//     },
+//     {
+//         imgurl : "https://rukminim1.flixcart.com/image/200/200/xif0q/refrigerator-new/q/z/s/-original-imagnjywjz7awf8h.jpeg?q=70",
+//            name : "Upgrade Your Kitchen",
+//            price : 10000,
+//            brand : "Extra 10% Off on bank cards"
+//     },
+//     {
+//         imgurl : "https://rukminim1.flixcart.com/image/200/200/xif0q/washing-machine-new/d/9/c/7-480-wtt70dbrtf-0-570-830-23-voltas-beko-1005-original-imagggbseufcbbnw.jpeg?q=70",
+//            name : "Best Budget Deals For You!",
+//            price : 9999,
+//            brand : "Under ₹10,000 "
+//     }
+// ]
+
+// for(let i = 0; i < topoffers.length ; i++){
+//     console.log(topoffers[0].name)
+//     let = renderProduct = `
+//     <div class = "card-product">
+//     <img src="${g}">
+//     <h3 class="card-product-name">Tempered Glass</h3>
+//     <p class="card-product-price">Under ₹99</p>
+//     <p class="card-product-desc">For All Top Models</p>
+//     </div>`
+// }
